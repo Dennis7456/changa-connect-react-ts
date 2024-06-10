48e1daffe2a3b9754b6f925a3f26c50acb14761c
Dennis Kiplangat <denniskiplangat.dk@gmail.com>
2024-06-11 00:33:06 +0300

    build: update dependencies

--- src/appollo.config.js
+++ src/appollo.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     client: {
         service: {
-            // url: "http://localhost:3001/graphql",
+            // url: "http://localhost:4000/graphql",
             // url: "https://changa-connect-react-ts.vercel.app/graphql",
             url: "",
             skipSSLValidation: true
